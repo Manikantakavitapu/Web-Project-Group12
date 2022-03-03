ec3bd134a6f3470956ad506597f468affd70602e
Manikanta Kavitapu <48171359+Manikantakavitapu@users.noreply.github.com>
2022-03-03 11:45:51 -0600

    intial commit

---
+++ petAdoption/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { BuyerComponent } from './buyer/buyer.component';
+import { HomeComponent } from './home/home.component';
+import { PetDetailsComponent } from './pet-details/pet-details.component';
+import { SellerComponent } from './seller/seller.component';
+
+const routes: Routes = [
+
+    {path:'home',component:HomeComponent},
+    { path: 'seller', component: SellerComponent },
+    { path:'login',component:LoginComponent},
+    { path:'signUp',component:SignupComponent},
+    {path:'buyer',component:BuyerComponent},
+    {path:'pet-details',component:PetDetailsComponent},
+    {path:'',redirectTo:'home', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ petAdoption/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SellerComponent } from './seller/seller.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { PetDetailsComponent } from './pet-details/pet-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SellerComponent,
+    AuthenticationComponent,
+    HomeComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    FooterComponent,
+    PetDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
